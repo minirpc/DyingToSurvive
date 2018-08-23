b432d5df7e9d3548eee71b681773dbd57883b663
change <zhibing.chen@baichanghui.com>
2018-08-23 15:40:36 +0800

    初始化项目　

---
+++ rpc-client/src/main/java/dyingtosurvive/com/App.java
@@ -0,0 +1,13 @@
+package dyingtosurvive.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
